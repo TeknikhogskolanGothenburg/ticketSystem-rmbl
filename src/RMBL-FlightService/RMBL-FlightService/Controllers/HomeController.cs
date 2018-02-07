1050594ed292e6f5de07fb88fe019cc8b22912bf
Z <zilvergamer@gmail.com>
2018-02-07 22:45:46 +0100

    Installed bootstrap 4 and connected and published it on Azure

---
+++ src/RMBL-FlightService/RMBL-FlightService/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using RMBL_FlightService.Models;
+
+namespace RMBL_FlightService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SQL()
+        {
+            var model = new DataBaseResp();
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "rmbl.database.windows.net";
+                builder.UserID = "rmblA";
+                builder.Password = "QAwsedrf123@@";
+                builder.InitialCatalog = "RMBL-SERVER";
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    //Console.WriteLine("Opening the port");
+                    connection.Open();
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT * FROM Test");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                model.Response.Add(reader.GetInt32(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            catch (SqlException e)
+            {
+                model.Response.Add(1, e.ToString());
+            }
+            return View("SQL", model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/RMBL-FlightService/RMBL-FlightService/Models/DataBaseResp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMBL_FlightService.Models
+{
+    public class DataBaseResp
+    {
+        public Dictionary<int, string> Response = new Dictionary<int, string>();
+    }
+}
