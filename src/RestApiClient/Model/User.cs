88babbf58e46cfd0735b81e58b5c8f5c9bf731aa
Rasmus Berg <chibi86@hotmail.com>
2018-02-08 16:05:41 +0100

    Add TicketShop project for real, add fields to User, Update BackOffice and TicketShop controllers
    TicketShop:
    Add: TicketShop project
    Move: Booking class to RestApiClient
    Rebuild: HomeController, add some actions
    
    BackOffice:
    Rebuild: HomeController, add some actions and change others
    
    RestApiClient:
    Add: Booking object class
    Rebuild: User object class, add some variables
    
    DbUp:
    Add: Fields to User table

--- src/RestApiClient/Model/User.cs
+++ src/RestApiClient/Model/User.cs
@@ -7,7 +7,13 @@ namespace TicketSystem.RestApiClient.Model
     public class User
     {
         public string Username { get; set; }
-        public string Password { private get; set; }
+        public string Password { get; set; }
+        public string Epost { get; set; }
         public Int16 Grade { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
     }
 }
