766e1e10f8e962a3fbae3232c0f4cac20aabbd1c
Rasmus Berg <chibi86@hotmail.com>
2018-02-12 13:38:43 +0100

    Rebuild Payment
    RestApiClient:
    Rebuild: Payment object class, add automatic OrderReference and set strict Valuta string.

--- src/RestApiClient/Model/Payment.cs
+++ src/RestApiClient/Model/Payment.cs
@@ -7,7 +7,18 @@ namespace TicketSystem.RestApiClient.Model
 {
     public class Payment
     {
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
+        [RegularExpression("^[A-Z]+$", ErrorMessage = "Only currency in select are accepted")]
         public string Valuta { get; set; }
-        public string OrderReference { get; set; }
+        public string OrderReference { get; private set; }
+
+        /// <summary>
+        /// Default constructor, make a order reference random 
+        /// </summary>
+        public Payment()
+        {
+            OrderReference = new Guid().ToString();
+        }
     }
 }
