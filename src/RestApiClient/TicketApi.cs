de74805a82232e02258121a6aea06e2950cddc71
Rasmus Berg <chibi86@hotmail.com>
2018-02-13 21:46:48 +0100

    Add AuthenticationLibrary project
    Add AuthenticationLibrary project
    
    ### RestApiClient:
    Add: Support for AuthenticationLibrary
    Move: Some code to AuthenticationLibrary

--- src/RestApiClient/TicketApi.cs
+++ src/RestApiClient/TicketApi.cs
@@ -2,9 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Security.Cryptography;
 using System.Text;
 using TicketSystem.RestApiClient.Model;
+using AuthenticationLibrary;
 
 namespace TicketSystem.RestApiClient
 {
@@ -198,16 +198,12 @@ namespace TicketSystem.RestApiClient
             RestClient client = new RestClient("https://rmbl-flightticketapi.azurewebsites.net");
 
             string timestamp = DateTime.Now.ToString("r");
-            string hashApiSecretTimestamp = HashMessageByKey(apiSecret, timestamp);
-
-            request.AddHeader("Authentication", apiKey + ":" + hashApiSecretTimestamp);
             request.AddHeader("Timestamp", timestamp);
+            request.AddHeader("Authentication", Authentication.AuthenticationHeader(apiKey, apiSecret, timestamp));
 
             if (sessionId != 0)
             {
-                string hashSessionSecretTimestamp = HashMessageByKey(sessionSecret, timestamp);
-
-                request.AddHeader("User-Authentication", sessionId + ":" + hashSessionSecretTimestamp);
+                request.AddHeader("User-Authentication", Authentication.UserAuthenticationHeader(sessionId, sessionSecret, timestamp));
             }
 
             return client;
@@ -236,20 +232,6 @@ namespace TicketSystem.RestApiClient
                 case HttpStatusCode.NoContent:
                     throw new NullReferenceException(string.Format("{0} {1} is not found", toWhat, withId));
             }
-        }
-
-        private string HashMessageByKey(string apiSecret, string message)
-        {
-            var key = Encoding.UTF8.GetBytes(apiSecret.ToUpper());
-            string hashString;
-
-            using (var hmac = new HMACSHA256(key))
-            {
-                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(message));
-                hashString = Convert.ToBase64String(hash);
-            }
-
-            return hashString;
         }
     }
 }
