35cba428da75c3432f077dd09153c519ecac8a52
Robert <zilvergamer@gmail.com>
2018-02-14 15:40:25 +0100

    Updating style files and added new view folder for the common user

---
+++ src/TicketShop/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Data.SqlClient;
+using TicketShop.Models;
+using TicketSystem.RestApiClient.Model;
+using TicketSystem.RestApiClient;
+
+namespace TicketShop.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        public IActionResult GetAllTickets()
+        {
+            return View();
+        }
+
+    }
+}
